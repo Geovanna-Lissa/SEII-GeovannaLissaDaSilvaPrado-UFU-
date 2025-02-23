d40bd533102ac16dd7005e82687778813cc5bcff
Geovanna Lissa <geovannalsprado@gmail.com>
2025-02-23 19:24:18 -0300

    TESTE

---
+++ semana03/.c
@@ -0,0 +1,21 @@
+string[] myArray = novo string[7];
+myArray[0] = "Segunda";
+myArray[1] = "Terca";
+myArray[2] = "Quarta";
+myArray[3] = "Quinta";
+myArray[4] = "Sexta";
+myArray[5] = "Sabado";
+myArray[6] = "Domingo";
+
+// os comandos acima movem os intens declarados para o Array.
+
+for (int i = 0; i < myArray.Length; i++)
+{
+
+    Console.Writeline(myArray[0]);
+
+// este comando serve para mostrar ual e o primeiro e que tem que ser lido 
+
+}
+
+// este comando faz um loop para ler todas as informacoes contidas no Array
