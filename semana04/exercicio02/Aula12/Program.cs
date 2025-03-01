6a859f1b0f88d10491409b151313a7d48f8f78f7
Geovanna-Lissa <geovannalsprado@gmail.com>
2025-03-01 19:44:52 -0300

    Add files via upload

---
+++ semana04/exercicio02/Aula12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Colaborador obj = new Colaborador("Geovanna", 22, 2000);
+        }
+    }
+}
---
+++ semana04/exercicio02/Aula14/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula14
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Exemplo.soma(3, 7);
+        }
+    }
+}
